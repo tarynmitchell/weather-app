f12685e5e0772aadc91225faf9b98be9482fb211
tarynmitchell <tarynmitchll@gmail.com>
2024-03-25 16:44:44 +0000

    Updated the javascript for the weather forecast

--- src/index.js
+++ src/index.js
@@ -82,21 +82,20 @@ function displayForecast(response) {
       forecastHtml =
         forecastHtml +
         `
-      <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.time)}</div>
-      
-        <img src="${
-          day.condition.icon_url
-        }" class="weather-forecast-icon"/></div>
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}º</strong>
-          </div>
-          <div class="weather-forecast-temperature">${Math.round(
-            day.temperature.minimum
-          )}º</div>
+    <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+      <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+          <strong>${Math.round(day.temperature.maximum)}º</strong>
+        </div>
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}º</div>
       </div>
-    `;
+    </div>
+  `;
     }
   });
 
